07ae7e09f86b1ff98da582d12636b4bd65491801
Mattia Sotgia <mattiasotgia01@gmail.com>
2021-02-05 09:20:18 +0100

    setup.py legibility

--- setup.py
+++ setup.py
@@ -41,172 +41,172 @@ if __name__ == "__main__":
 
         folder_full_path = current_path + '/' + title_underscore
 
-try:
-    os.mkdir(folder_full_path)
-except OSError:
-    logging.exception('Creation of directory {} failed'.format(folder_full_path))
-else:
-    logging.info('Path {} created successfully'.format(folder_full_path))
-    
-os.chdir(folder_full_path)
-
-readme_file = open('README.md', 'w')
-print('''README file
-===========
-
-Folder structure
-----------------
-main folder /{}
-
-> /relazione
-    for all pdf versions of paper
-    also for all .tex and .docx eventually
-> /fig
-    for everythong concerning .figs, .pdfs, .ai's
-> /dati
-    raw and polished data goes here.
-> /analisi_dati
-    everuthing __code__ goes here.
-> /misc
-    everything else (log files, results to be 
-    copied)
-
-'''.format(title_underscore), file=readme_file)
-
-paths = [
-   '/fig',
-   '/relazione',
-   '/dati',
-   '/analisi_dati',
-   '/misc'
-]
-
-for i in paths:
     try:
-        os.mkdir(folder_full_path + i)
+        os.mkdir(folder_full_path)
     except OSError:
-        logging.exception('Creation of directory {} failed'.format(folder_full_path + i))
+        logging.exception('Creation of directory {} failed'.format(folder_full_path))
     else:
-        logging.info('Path {} created successfully'.format(folder_full_path + i))
+        logging.info('Path {} created successfully'.format(folder_full_path))
+        
+    os.chdir(folder_full_path)
+
+    readme_file = open('README.md', 'w')
+    print('''README file
+    ===========
+
+    Folder structure
+    ----------------
+    main folder /{}
+
+    > /relazione
+        for all pdf versions of paper
+        also for all .tex and .docx eventually
+    > /fig
+        for everythong concerning .figs, .pdfs, .ai's
+    > /dati
+        raw and polished data goes here.
+    > /analisi_dati
+        everuthing __code__ goes here.
+    > /misc
+        everything else (log files, results to be 
+        copied)
+
+    '''.format(title_underscore), file=readme_file)
+
+    paths = [
+    '/fig',
+    '/relazione',
+    '/dati',
+    '/analisi_dati',
+    '/misc'
+    ]
+
+    for i in paths:
+        try:
+            os.mkdir(folder_full_path + i)
+        except OSError:
+            logging.exception('Creation of directory {} failed'.format(folder_full_path + i))
+        else:
+            logging.info('Path {} created successfully'.format(folder_full_path + i))
 
 
-os.chdir(folder_full_path + paths[1])
+    os.chdir(folder_full_path + paths[1])
 
 
-title_full = input('Enter full title Latex: ')
+    title_full = input('Enter full title Latex: ')
 
 
-template_file = open('esperienza_{}_{}'.format(esp_no, now.strftime('%Y_%m_%d')) + '.tex', 'w')
+    template_file = open('esperienza_{}_{}'.format(esp_no, now.strftime('%Y_%m_%d')) + '.tex', 'w')
 
 
-tex_template = '''\\documentclass[italian, a4paper, 10pt, twocolumn]{{../../style/lab_unige_v2}}
-\\usepackage[a4paper, margin=1.25cm, footskip=0.25in]{{geometry}}
+    tex_template = '''\\documentclass[italian, a4paper, 10pt, twocolumn]{{../../style/lab_unige_v2}}
+    \\usepackage[a4paper, margin=1.25cm, footskip=0.25in]{{geometry}}
 
-\\usepackage[utf8]{{inputenc}}
-\\usepackage[T1]{{fontenc}}
+    \\usepackage[utf8]{{inputenc}}
+    \\usepackage[T1]{{fontenc}}
 
-\\usepackage[italian]{{babel}}
+    \\usepackage[italian]{{babel}}
 
-% \\usepackage{{biblatex}}
+    % \\usepackage{{biblatex}}
 
-\\usepackage[bookmarksopen=true, 
-citebordercolor={{0 1 0}}, 
-linkbordercolor={{1 0 0}}, 
-urlbordercolor={{0 1 1}}]{{hyperref}}
-\\usepackage[numbered]{{bookmark}}
+    \\usepackage[bookmarksopen=true, 
+    citebordercolor={{0 1 0}}, 
+    linkbordercolor={{1 0 0}}, 
+    urlbordercolor={{0 1 1}}]{{hyperref}}
+    \\usepackage[numbered]{{bookmark}}
 
-\\usepackage{{graphicx}}
-\\usepackage{{array}}
-\\usepackage{{tabulary}}
-\\usepackage{{booktabs}}
+    \\usepackage{{graphicx}}
+    \\usepackage{{array}}
+    \\usepackage{{tabulary}}
+    \\usepackage{{booktabs}}
 
-% FOUNDAMENTAL
-\\usepackage{{../../style/custom}}
+    % FOUNDAMENTAL
+    \\usepackage{{../../style/custom}}
 
-\\usepackage{{physics}}
+    \\usepackage{{physics}}
 
-\\usepackage{{breqn}}
-\\usepackage{{cuted}}
-\\usepackage{{txfonts}}
+    \\usepackage{{breqn}}
+    \\usepackage{{cuted}}
+    \\usepackage{{txfonts}}
 
-\\usepackage{{lipsum}}
+    \\usepackage{{lipsum}}
 
-%% Define ref types
-\\newcommand{{\\reftab}}[1]{{Tab. {{\\ref{{#1}}}}}}%
-\\newcommand{{\\reffig}}[1]{{Fig. {{\\ref{{#1}}}}}}%
-\\newcommand{{\\refeqn}}[1]{{({{\\ref{{#1}})}}}}%
-%%
-\\setlength{{\\columnsep}}{{6mm}}
-\\begin{{document}}
+    %% Define ref types
+    \\newcommand{{\\reftab}}[1]{{Tab. {{\\ref{{#1}}}}}}%
+    \\newcommand{{\\reffig}}[1]{{Fig. {{\\ref{{#1}}}}}}%
+    \\newcommand{{\\refeqn}}[1]{{({{\\ref{{#1}})}}}}%
+    %%
+    \\setlength{{\\columnsep}}{{6mm}}
+    \\begin{{document}}
 
 
-\\twocolumn[
-  \\begin{{@twocolumnfalse}}
+    \\twocolumn[
+    \\begin{{@twocolumnfalse}}
+        
+        \\title{{
+            {}
+        }}
+        \\author{{
+        Eugenio Dormicchi\\textsuperscript{{1}},  
+        Riccardo Pizzimbone\\textsuperscript{{1}}, 
+        Mattia Sotgia\\textsuperscript{{1}}
+        }}
+
+        \\date{{
+        \\textsuperscript{{1}}Gruppo C03, Esperienza di laboratorio n. {}\\\\
+        %\\textsuperscript{{2}}In presenza in laboratorio per la presa dati\\\\
+            % Università degli Studi di Genova, Dipartimento di Fisica.\\\\
+            Presa dati–– 
+            {}, 15:00– 18:00; Analisi dati–– 
+            <end-date here>
+        }}
+        \\maketitle
+        
+        \\begin{{abstract}}
+        
+        \\textit{{Obiettivo– }}
+        
+        \\textit{{Metodi– }}
+        
+        \\textit{{Risultati– }}
+        
+        \\textit{{Conclusione– }}
+        
+        
+        \\end{{abstract}}
+        \\vspace{{2em}}
+        \\end{{@twocolumnfalse}}
+    ]
+
+    %%%% CORPO DEL TESTO
+    %%%% CORPO DEL TESTO
+
+    \\section{{Obiettivo}}
+    \\label{{section:aim}}
+
+    \\section{{Strumentazione}}
+    \\label{{section:strument}}
+
+    \\section{{Metodi}}
+    \\label{{section:methods}}
+
+    \\section{{Risultati}}
+    \\label{{section:results}}
+
+    \\section{{Conclusione}}
+    \\label{{section:conclusion}}
+
+    \\subsection{{Controlli}}
+
+    \\subsection{{Possibili errori sistematici}}
     
-    \\title{{
-        {}
-    }}
-    \\author{{
-      Eugenio Dormicchi\\textsuperscript{{1}},  
-      Riccardo Pizzimbone\\textsuperscript{{1}}, 
-      Mattia Sotgia\\textsuperscript{{1}}
-    }}
 
-    \\date{{
-      \\textsuperscript{{1}}Gruppo C03, Esperienza di laboratorio n. {}\\\\
-      %\\textsuperscript{{2}}In presenza in laboratorio per la presa dati\\\\
-    	% Università degli Studi di Genova, Dipartimento di Fisica.\\\\
-    	Presa dati–– 
-    	{}, 15:00– 18:00; Analisi dati–– 
-    	<end-date here>
-    }}
-    \\maketitle
-    
-    \\begin{{abstract}}
-      
-      \\textit{{Obiettivo– }}
-      
-      \\textit{{Metodi– }}
-      
-      \\textit{{Risultati– }}
-      
-      \\textit{{Conclusione– }}
-      
-      
-    \\end{{abstract}}
-    \\vspace{{2em}}
-    \\end{{@twocolumnfalse}}
-]
+    \\end{{document}}
+    '''.format(title_full, esp_no, now.strftime('%d %B %Y'))
 
-  %%%% CORPO DEL TESTO
-  %%%% CORPO DEL TESTO
+    print(tex_template, file=template_file)
 
-  \\section{{Obiettivo}}
-  \\label{{section:aim}}
+    logging.info('Done')
+    print('Done, see log file for errors!')
 
-  \\section{{Strumentazione}}
-  \\label{{section:strument}}
-
-  \\section{{Metodi}}
-  \\label{{section:methods}}
-
-  \\section{{Risultati}}
-  \\label{{section:results}}
-
-  \\section{{Conclusione}}
-  \\label{{section:conclusion}}
-
-  \\subsection{{Controlli}}
-
-  \\subsection{{Possibili errori sistematici}}
-  
-
-\\end{{document}}
-'''.format(title_full, esp_no, now.strftime('%d %B %Y'))
-
-print(tex_template, file=template_file)
-
-logging.info('Done')
-print('Done, see log file for errors!')
-
-# end of document
+    # end of document
